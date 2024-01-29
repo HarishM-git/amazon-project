88c07d124164dc2b227dea143291d8074aeb21fb
harishm-git <harishdeepakh@gmail.com>
2024-01-29 17:06:21 +0530

    added space

--- scripts/checkout/paymentsummary.js
+++ scripts/checkout/paymentsummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getproduct } from "../../data/products.js";
 let productprice=0;
 
+
 export function renderpaymentsummary(){
   cart.forEach(cartitem => {
     const product= getproduct(cartitem.productId);
