b1eece61ad18c28dec0dcf0a784417774b014d96
harishm-git <harishdeepakh@gmail.com>
2024-01-29 17:07:21 +0530

    special add2

--- scripts/checkout/paymentsummary.js
+++ scripts/checkout/paymentsummary.js
@@ -2,7 +2,6 @@ import { cart } from "../../data/cart.js";
 import { getproduct } from "../../data/products.js";
 let productprice=0;
 
-
 export function renderpaymentsummary(){
   cart.forEach(cartitem => {
     const product= getproduct(cartitem.productId);
