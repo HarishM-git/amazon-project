672e393a12a9e7a6ea3891907a2c2ff439c559e8
harishm-git <harishdeepakh@gmail.com>
2024-01-30 19:31:26 +0530

    doing the calculation part in js

--- scripts/checkout/paymentsummary.js
+++ scripts/checkout/paymentsummary.js
@@ -1,6 +1,7 @@
 import { cart } from "../../data/cart.js";
 import { getproduct } from "../../data/products.js";
 import {getdeliveryoption} from '../../data/deliveryoptions.js'
+import { tofixedpricecents } from "../utils/money.js";
 let productprice=0;
 let shippingproductprice=0;
 
@@ -11,9 +12,52 @@ export function renderpaymentsummary(){
     const deliveryoption=getdeliveryoption(cartitem.deliveryoptionID);
     shippingproductprice+=deliveryoption.pricecents;
   });
-  console.log(productprice);
-  console.log(shippingproductprice);
+  const totalbeforetax=productprice+shippingproductprice;
+  const tax=totalbeforetax*0.2;
+  const totalaftertax=totalbeforetax+tax;
+  console.log(totalaftertax);
+  const paymentsummaryHTML=`
+          <div class="payment-summary">
+                  <div class="payment-summary-title">
+                    Order Summary
+                  </div>
 
+                  <div class="payment-summary-row">
+                    <div>Items (3):</div>
+                    <div class="payment-summary-money">${tofixedpricecents(productprice)}</div>
+                  </div>
+
+                  <div class="payment-summary-row">
+                    <div>Shipping &amp; handling:</div>
+                    <div class="payment-summary-money">
+                    $${tofixedpricecents(shippingproductprice)}
+                    </div>
+                  </div>
+
+                  <div class="payment-summary-row subtotal-row">
+                    <div>Total before tax:</div>
+                    <div class="payment-summary-money">
+                    $${tofixedpricecents(totalbeforetax)}
+                    </div>
+                  </div>
+
+                  <div class="payment-summary-row">
+                    <div>Estimated tax (10%):</div>
+                    <div class="payment-summary-money">
+                    $${tofixedpricecents(tax)}
+                    </div>
+                  </div>
+
+                  <div class="payment-summary-row total-row">
+                    <div>Order total:</div>
+                    <div class="payment-summary-money">$${tofixedpricecents(totalaftertax)}</div>
+                  </div>
+
+                  <button class="place-order-button button-primary">
+                    Place your order
+                  </button>  
+  `
+  
 
 
 
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,3 @@
 export function tofixedpricecents(pricecent){
-  return  (pricecent/100).toFixed(2);
+  return  (Math.round(pricecent)/100).toFixed(2);
 }
