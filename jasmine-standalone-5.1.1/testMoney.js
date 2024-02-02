9239a32454c7ebb7662907eb758e5bc40bb4cc2d
harishm-git <harishdeepakh@gmail.com>
2024-02-02 20:27:45 +0530

    sucessfully checked second test case

--- jasmine-standalone-5.1.1/testMoney.js
+++ jasmine-standalone-5.1.1/testMoney.js
@@ -2,8 +2,11 @@ import { tofixedpricecents } from "../scripts/utils/money.js";
 
 
 describe('testing group for money.js:',()=>{
-  it('converts pricecents to dollars',()=>{
+  it('converts pricecents to dollars:',()=>{
     expect(tofixedpricecents(2005)).toEqual('20.05');
+  });
+  it('checks for zero:',()=>{
+    expect(tofixedpricecents(0)).toEqual('0.00');
   });
   
 });
