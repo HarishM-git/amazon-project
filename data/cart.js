faa6e39adff2bfb2e8411a8c21b72cbb2d3f841d
harishm-git <harishdeepakh@gmail.com>
2024-02-06 20:29:26 +0530

    working on add to cart function

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,16 @@
-export let cart=JSON.parse(localStorage.getItem('cart'))||[];
+export let cart;
+export function loadfromstorage(){
+  cart=JSON.parse(localStorage.getItem('cart'))||[{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:1,
+    deliveryoptionID:'1'
+  },
+  {
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:2,
+    deliveryoptionID:'2'
+  }];
+}
 
 
 
--- jasmine-standalone-5.1.1/data/testCart.js
+++ jasmine-standalone-5.1.1/data/testCart.js
@@ -1,7 +1,14 @@
 import { addtoCart ,cart} from "../../data/cart.js";
 
 describe('test suite add to cart:',()=>{
+
+
   it('testing add to cart function weather it adds or not:',()=>{
+    spyOn( localStorage,'getItem').and.callFake(()=>{
+      return JSON.stringify([]);
+  
+    })
+    console.log(localStorage.getItem('cart'));
     addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
     expect(cart.length).toEqual(1);
   })
