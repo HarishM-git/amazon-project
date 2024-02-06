1b6119dadd4504335aa19169c53ceab54930db2f
harishm-git <harishdeepakh@gmail.com>
2024-02-06 20:37:56 +0530

    yeah now its going on with localstorage .getitem next commit will be for testing the localstorage.setitem

--- jasmine-standalone-5.1.1/data/testCart.js
+++ jasmine-standalone-5.1.1/data/testCart.js
@@ -1,4 +1,4 @@
-import { addtoCart ,cart} from "../../data/cart.js";
+import { addtoCart ,loadfromstorage,cart} from "../../data/cart.js";
 
 describe('test suite add to cart:',()=>{
 
@@ -6,8 +6,8 @@ describe('test suite add to cart:',()=>{
   it('testing add to cart function weather it adds or not:',()=>{
     spyOn( localStorage,'getItem').and.callFake(()=>{
       return JSON.stringify([]);
-  
     })
+    loadfromstorage();
     console.log(localStorage.getItem('cart'));
     addtoCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
     expect(cart.length).toEqual(1);
