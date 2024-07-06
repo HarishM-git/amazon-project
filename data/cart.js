9d015fcd75b3251c59ab56d4e018edc9131aa089
harishm-git <harishdeepakh@gmail.com>
2024-07-06 22:59:43 +0530

    trying to create a sign in form

--- data/cart.js
+++ data/cart.js
@@ -24,15 +24,10 @@ function savetostorage(){
 
 
 export function addtoCart(productId){
-
-
   let matchingId;
-
   cart.forEach(cartitem => {
     if (productId===cartitem.productId){
-
       matchingId=cartitem;
-      
   }
  
   }); 
