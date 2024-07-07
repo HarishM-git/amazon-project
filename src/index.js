5301c948949f3642edfca1eda300f6276cb22c07
harishm-git <harishdeepakh@gmail.com>
2024-07-07 19:08:44 +0530

    today i finished 75% of the work

--- src/index.js
+++ src/index.js
@@ -1,53 +1,63 @@
-const express=require("express");
-const app=express();
-const path=require("path")
-const hbs=require("hbs")
-const templatePath=path.join(__dirname,"../templates")
-const collection=require("./mongodb")
-app.use(express.json())
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const collection = require("./mongodb");
 
-app.set("view engine","hbs") 
-app.set("views",templatePath)
-app.use(express.urlencoded({extended:false}))
-
-app.get("/",(req,res)=>{
-  res.render("login")
-})
-app.get("/signup",(req,res)=>{
-  res.render("signup")
-})
+const templatePath = path.join(__dirname, "../templates");
+const rootPath = path.join(__dirname, "..");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 
-app.post("/signup",async(req,res)=>{
-  const data={
-    name:req.body.name,
-    password:req.body.password
+
+
+
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.name.trim(),
+    password: req.body.password.trim(),
+  };
+  
+  try {
+    await collection.insertMany([data]);
+    res.sendFile(path.join(rootPath, "index.html")); 
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).send("An error occurred during signup");
   }
-  await collection.insertMany([data])
-  res.render("home")
+});
 
-})
+app.post("/login", async (req, res) => {
+  const userName = req.body.name.trim();
+  const userPass = req.body.password.trim();
 
-app.post("/login",async(req,res)=>{
-  try{
-    const check=await collection.findOne({name:req.body.name})
-    if(check.password===req.body.password){
-      res.render("home")
+  try {
+    const check = await collection.findOne({ name: userName });
+    
+    if (check && check.password === userPass) {
+      res.sendFile(path.join(rootPath, "index.html")); 
+    } else {
+      res.send("Wrong username or password");
     }
-    else{
-      res.send("wrong password")
-    }
-  
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).send("An error occurred during login");
   }
-  catch{
-    res.send("wrond details")
-  }
-  
-  
+});
 
-})
-
-app.listen(3000,()=>{
-   console.log("port connected")
-})
+app.listen(3000, () => {
+  console.log("Port connected");
+});
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb+srv://harishdeepakh:t8kdiX7fv03E30Xx@cluster0.1vk97rc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb://localhost:27017/loginSignupTUT")
 .then(()=>{
   console.log("mongodb connected yhahoo")
 })
